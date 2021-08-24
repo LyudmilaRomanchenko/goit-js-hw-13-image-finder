7d93a3cf50991fe82d1ee706322479c03350c115
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-08-24 16:33:47 +0300

    Добавляет ссылки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+
+// import formTemplates from './templates/form';
+// console.log(formTemplates);
+
+import ApiService from './js/apiService';
+console.log(ApiService);
+
+import debounce from 'lodash.debounce';
+
+
+
+const newApiService = new ApiService;
+newApiService.fetchImg();
+
+const refs = {
+    getForm: document.querySelector('.search-form'),
+}
+console.log(refs.getForm);
+
+refs.getForm.addEventListener('input', debounce(onSearch, 5000));
+
+function onSearch(event) {
+    event.preventDefault();
+    console.log('hhhhhhhhh');
+}
+  
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,3 +1,18 @@
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '23052937-32fb9bd6f4b84b12682be3748';
 
+export default class ApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchImg() {
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=cat&page=${this.page}&per_page=12&key=${API_KEY}`;
+        return fetch(url).then(response => response.json()
+        .then(data => console.log(data))
+        );
+    }
+
+}
+
