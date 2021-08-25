654fb5f9cf2d2a9e168b33fd1a0ea434c64f697e
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-08-25 12:55:40 +0300

    Добавляет рендер страницы

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-// import './sass/main.scss';
+import './sass/main.scss';
 
-// import formTemplates from './templates/form';
-// console.log(formTemplates);
+import galleryTemplates from './templates/gallery';
+console.log(galleryTemplates);
+
+import cardTemplates from './templates/card';
+console.log(cardTemplates);
 
 import ApiService from './js/apiService';
 console.log(ApiService);
@@ -11,17 +14,63 @@ import debounce from 'lodash.debounce';
 
 
 const newApiService = new ApiService;
-newApiService.fetchImg();
 
 const refs = {
-    getForm: document.querySelector('.search-form'),
+    getInput: document.querySelector('.search-input'),
+    getGalleryList: document.querySelector('.gallery-list'),
+    getGalleryListItem: document.querySelector('.gallery-item'),
+    getCard: document.querySelector('.photo-cardist'),
+    
 }
-console.log(refs.getForm);
+console.log(refs.getInput);
 
-refs.getForm.addEventListener('input', debounce(onSearch, 5000));
+refs.getInput.addEventListener('input', debounce(onSearch, 1000));
 
 function onSearch(event) {
     event.preventDefault();
-    console.log('hhhhhhhhh');
+
+    newApiService.query = refs.getInput.value;
+    console.log(newApiService.query);
+
+    if (newApiService.query.trim() === '') {
+        return;
+    }
+
+    newApiService.resetPage();
+    clearGalleryList();
+    
+
+
+    newApiService.fetchImg().then(gallery => {
+        console.log(gallery);
+        renderGalleryList(gallery);
+    
+    })
+
+     
+
+    console.log(newApiService.fetchImg());
+    
 }
+
+function renderGalleryList(gallery) {
+    const markUp = galleryTemplates(gallery);
+    refs.getGalleryList.innerHTML = markUp;
+}
+
+function clearGalleryList() {
+  refs.getGalleryList.innerHTML = '';
+}
+
+
+// для кнопки показать еще
+function scroll() {
+    const element = document.getElementById('my-element-selector');
+
+    element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    });
+}
+
   
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,10 +8,29 @@ export default class ApiService {
     }
 
     fetchImg() {
-        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=cat&page=${this.page}&per_page=12&key=${API_KEY}`;
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
         return fetch(url).then(response => response.json()
-        .then(data => console.log(data))
+            .then(data => {
+                console.log(data);
+                return data.hits;
+            })
         );
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
     }
 
 }
